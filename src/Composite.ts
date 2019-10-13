58ca53a7a69747c0329e60e258282bb6b1c65c35
Hamachan <hamachan@Hamachans-MacBook-Pro.local>
2019-10-13 20:06:09 +0700

    composite

---
+++ src/Composite.ts
@@ -0,0 +1,64 @@
+abstract class Component {
+    protected parent: Component
+
+    setParent(parent: Component): void {
+        this.parent = parent
+    }
+
+    getParent(): Component {
+        return this.parent
+    }
+
+    add(component: Component): void {}
+
+    remove(component: Component): void {}
+
+    isComposite(): boolean {
+        return false
+    }
+
+    abstract operation(): string 
+}
+
+class Leaf extends Component {
+    operation(): string {
+        return 'Leaf'
+    }
+}
+
+class Composite extends Component {
+    protected children: Component[] = []
+
+    add(component: Component): void {
+        this.children.push(component)
+        component.setParent(this)
+    }
+
+    remove(component: Component): void {
+        const componentIndex = this.children.indexOf(component)
+        this.children.splice(componentIndex, 1)
+        component.setParent(undefined)
+    }
+
+    isComposite(): boolean {
+        return true
+    }
+
+    operation(): string {
+        const results = []
+        for (const child of this.children) {
+            results.push(child)
+        }
+        return `Branch ${results.join(' + ')}`
+    }
+}
+
+const simple = new Leaf()
+const tree = new Composite()
+const branch1 = new Composite()
+branch1.add(new Leaf())
+branch1.add(new Leaf())
+const branch2 = new Composite()
+branch2.add(new Leaf())
+tree.add(branch1)
+tree.add(branch2)
